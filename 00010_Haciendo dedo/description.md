8086d9e49905fc97af40ea955b7ff488a31713c8
franco@mumuki.org <franco@mumuki.org>
2016-06-03 16:15:58 -0400

    Mumuki Export on 2016-06-03 16:15:58 -0400

--- 00010_Haciendo dedo/description.md
+++ 00010_Haciendo dedo/description.md
@@ -1,3 +1,3 @@
-Parece que el `not/1` es fácil de usar, ¿no? Pongamos lo que acabamos de aprender a prueba:
+Parece que el `not/1` es fácil de usar, ¿no? Pongamos a prueba lo que acabamos de aprender:
 
 > Escribí un predicado `loLLeva/2`, que relaciona a dos personas cuando la primera puede llevar a la segunda; esto se da cuando son vecinos, la primera tiene auto y la segunda no. 
--- 00010_Haciendo dedo/extra.pl
+++ 00010_Haciendo dedo/extra.pl
@@ -1,4 +1,34 @@
+viveEn(mariano, avellaneda).
+viveEn(fede, avellaneda).
+viveEn(victoria, versalles).
+viveEn(rodrigo, villaBallester).
+viveEn(tomas, nuniez).
+
+quedaEn(utn, almagro).
+quedaEn(utn, villaLugano).
+quedaEn(exactas, nuniez).
+quedaEn(river, nuniez).
+quedaEn(racing, avellaneda). 
+quedaEn(montoto, palermo).
+quedaEn(montoto, nuniez).
+quedaEn(montoto, avellaneda).
+
+tieneAuto(tomas).
+tieneAuto(fede). 
+
+llegaFacil(batman, _).
+llegaFacil(Persona, _) :-
+ tieneAuto(Persona).
+llegaFacil(Persona, Destino) :-
+  viveEn(Persona, Zona),
+  quedaEn(Destino, Zona).
+  
+destinosProximos(Destino, OtroDestino) :-
+   quedaEn(Destino, Zona),
+   quedaEn(OtroDestino, Zona).
+   
 sonVecinos(UnaPersona, OtraPersona) :-
-   viveEn(UnaPersona, Lugar),
-   viveEn(OtraPersona, Lugar),
-   UnaPersona \= OtraPersona.
+    UnaPersona \= OtraPersona,
+    viveEn(UnaPersona, Zona), 
+    viveEn(OtraPersona, Zona).
+    

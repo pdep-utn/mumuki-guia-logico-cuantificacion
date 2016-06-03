5eaa83580e493b27f05a33e6e00fc35139e91a5f
franco@mumuki.org <franco@mumuki.org>
2016-06-03 16:13:52 -0400

    Mumuki Export on 2016-06-03 16:13:52 -0400

---
+++ 00009_¡No, no y no!/extra.pl
@@ -0,0 +1,33 @@
+viveEn(mariano, avellaneda).
+viveEn(fede, avellaneda).
+viveEn(victoria, versalles).
+viveEn(rodrigo, villaBallester).
+viveEn(tomas, nuniez).
+
+quedaEn(utn, almagro).
+quedaEn(utn, villaLugano).
+quedaEn(exactas, nuniez).
+quedaEn(river, nuniez).
+quedaEn(racing, avellaneda). 
+quedaEn(montoto, palermo).
+quedaEn(montoto, nuniez).
+quedaEn(montoto, avellaneda).
+
+tieneAuto(tomas).
+tieneAuto(fede). 
+
+llegaFacil(batman, _).
+llegaFacil(Persona, _) :-
+ tieneAuto(Persona).
+llegaFacil(Persona, Destino) :-
+  viveEn(Persona, Zona),
+  quedaEn(Destino, Zona).
+  
+destinosProximos(X, Y) :-
+   quedaEn(X, Z),
+   quedaEn(Y, Z).
+   
+sonVecinos(X, Y) :-
+    X \= Y,
+    viveEn(X, Z), 
+    viveEn(Y, Z).

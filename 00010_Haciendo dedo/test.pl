b9097c75c34bab9b7ea83bbc0062f15ad9d50dfe
franco@mumuki.org <franco@mumuki.org>
2016-06-03 16:19:47 -0400

    Mumuki Export on 2016-06-03 16:19:47 -0400

--- 00010_Haciendo dedo/test.pl
+++ 00010_Haciendo dedo/test.pl
@@ -1,1 +1,11 @@
-todo
+test('no puedeLlevar(mariano, fede)'):-
+  \+ puedeLlevar(mariano, fede).
+  
+test('no puedeLlevar(fede, fede)'):-
+  \+ puedeLlevar(fede, fede).
+  
+test('puedeLlevar(fede, mariano)'):-
+  puedeLlevar(fede, mariano).
+  
+test('no puedeLlevar(fede, victoria)'):-
+  \+ puedeLlevar(fede, victoria).

36fcd5d5de4a76451bb4040b2fb4a646f1b19dab
franco@mumuki.org <franco@mumuki.org>
2016-06-03 15:50:02 -0400

    Mumuki Export on 2016-06-03 15:50:02 -0400

--- 00004_Existencia, parte 2/extra.pl
+++ 00004_Existencia, parte 2/extra.pl
@@ -1,17 +1,18 @@
-tieneAuto(tomas).
-tieneAuto(fede).
-
-viveEn(victoria, versalles).
 viveEn(mariano, avellaneda).
 viveEn(fede, avellaneda).
+viveEn(victoria, versalles).
+viveEn(rodrigo, villaBallester).
 viveEn(tomas, nuniez).
 
+quedaEn(utn, almagro).
+quedaEn(utn, villaLugano).
 quedaEn(exactas, nuniez).
+quedaEn(river, nuniez).
+quedaEn(racing, avellaneda). 
 
+tieneAuto(tomas).
+tieneAuto(fede). 
+
+llegaFacil(Conductor, _) :-
+   tieneAuto(Conductor).
 llegaFacil(batman, _).
-llegaFacil(Persona, _) :-
- tieneAuto(Persona).
-llegaFacil(Persona, Destino) :-
-  viveEn(Persona, Zona),
-  quedaEn(Destino, Zona).
- 

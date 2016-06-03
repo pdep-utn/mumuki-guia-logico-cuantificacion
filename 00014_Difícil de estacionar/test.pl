0eb39458f7851cbc49502b9ce0c5f23af077effb
franco@mumuki.org <franco@mumuki.org>
2016-06-03 16:55:10 -0400

    Mumuki Export on 2016-06-03 16:55:10 -0400

--- 00014_Difícil de estacionar/test.pl
+++ 00014_Difícil de estacionar/test.pl
@@ -1,1 +1,8 @@
-todo
+test('dificilDeEstacionar(nuniez)') :-.
+  dificilDeEstacionar(nuniez).
+  
+test('no dificilDeEstacionar(avellaneda)') :-.
+  \+ dificilDeEstacionar(avellaneda).
+  
+test('no dificilDeEstacionar(versalles)') :-.
+  \+ dificilDeEstacionar(versalles).

84e6a553a4d35797ff90fe51b50e679fae4508bb
mmatos87@gmail.com <mmatos87@gmail.com>
2017-01-26 16:50:08 -0500

    Mumuki Export on 2017-01-26 16:50:08 -0500

--- 00014_Difícil de estacionar/test.pl
+++ 00014_Difícil de estacionar/test.pl
@@ -6,3 +6,6 @@ test('no dificilDeEstacionar(avellaneda)') :-
   
 test('no dificilDeEstacionar(versalles)') :-
   \+ dificilDeEstacionar(versalles).
+  
+test('no dificilDeEstacionar(almagro)') :-
+  \+ dificilDeEstacionar(almagro).
